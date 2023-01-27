66f193656cc8a832e6272c6bf0de97ae5c8912da
“Irina <irina.osipova2012@gmail.com>
2023-01-27 16:25:34 +0200

    feat: function complete

--- src/index.js
+++ src/index.js
@@ -39,6 +39,34 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+  //make array with expr div by 10 items: ['0010101010', ..]
+const arrDouble = [];
+while (arrDouble.length < expr.length / 10 | 0) {
+  arrDouble.push(expr.substring(10*(arrDouble.length), 10*(arrDouble.length+1)));
+}  
+//make array where 10 numbers div 2 numbers: [ '00', '10', '10', '10', '10' ] [ '00', '00', '00', '00', '10' ] ...
+const arrResult = [];
+
+arrDouble.map(function(item) {
+  const arrTwo = [];
+  while (arrTwo.length < item.length / 2 | 0) {
+    arrTwo.push(item.substring(2*(arrTwo.length), 2*(arrTwo.length+1)))  //done [ '00', '10', '10', '10', '10' ] -1arr
+  }
+  let keyMorse = '';
+  for (i=0; i<arrTwo.length; i++) {
+    keyMorse = keyMorse+arrTwo[i].replace ('10', '.').replace ('11', '-').replace ('00', ''); // str из . -
+
+  }
+ letter = MORSE_TABLE[keyMorse]; 
+  if (letter === undefined) {
+    letter = ' ';
+  }
+  arrResult.push(letter);
+  })
+  
+  let result = arrResult.join('');
+  return result;
+
 }
 
 module.exports = {
